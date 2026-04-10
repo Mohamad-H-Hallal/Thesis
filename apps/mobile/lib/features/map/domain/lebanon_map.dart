f7dfd83a9bd451b4b0b4350ad4b09465a95d831e
Mohamad-H-Hallal <hallalmohamad49@gmail.com>
2026-04-10 20:23:55 +0300

    fix(map): tune project map street basemap

--- apps/mobile/lib/features/map/domain/lebanon_map.dart
+++ apps/mobile/lib/features/map/domain/lebanon_map.dart
@@ -59,7 +59,7 @@ class LebanonMapConfig {
       case LebanonBasemapStyle.satellite:
         return 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
       case LebanonBasemapStyle.street:
-        return 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}';
+        return 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
     }
   }
 
